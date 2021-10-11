1a3b88ca807a858e64b7402c48533eef9b3e12e4
Divyam Pal <85020103+DivyamPal@users.noreply.github.com>
2021-10-11 22:32:06 +0530

    Adding files

--- day_14/routes/index.js
+++ day_14/routes/index.js
@@ -15,23 +15,6 @@ router.get('/test', function(req, res, next) {
   console.log("Redis Value", req.session.username);
   res.render('index', { title: 'Express' });
 });
- 
- 
-/**
- * @requires {email, password, confirmPassword} - req.body
- * @description
- * Security, performance and edge cases
- * level - 01
- * email validate - @.
- * password validate
- * level - 02
- * JS / SQL
- * level - 03
- * check if email already ecists
- * password hash
- * email lowercase
- * save
- */
 
 router.post('/register', registerInitialCheck, register)
 router.post('/register-super-admin', registerInitialCheck, registerSuperAdmin)
